e3e1a022aae71ba85c393cd803bced309573a5d0
Benjamin Urbina <102681531+BenjaminUrbina@users.noreply.github.com>
2025-06-26 15:33:29 -0400

    Subiendo fase 3 del automatas

--- Compilador/pruebas.py
+++ Compilador/pruebas.py
@@ -2,25 +2,27 @@ import json
 from lexer.lexer import tokenize
 from parser.parser import Parser
 from utility.print import print_ast
+from samantica.semantic_analyzer import analyze
 
 if __name__ == "__main__":
-    with open("examples/miniscript.ms") as f:
+    with open("Compilador/examples/miniscript.ms") as f:
         code = f.read()
 
     tokens = tokenize(code)
     parser = Parser(tokens)
-    
+
     try:
         ast = parser.parse()
         print("✅ Análisis sintáctico completado.")
         
-        with open("output/ast.json", "w") as f:
+        with open("Compilador/output/ast.json", "w") as f:
             json.dump(ast, f, indent=2)
 
-        print("📄 AST guardado en output/ast.json")
+        print("📄 AST guardado en Compilador/output/ast.json")
+
+        # 🔍 Análisis semántico
+        print("\n🧠 Iniciando análisis semántico...")
+        analyze(ast)
+
     except Exception as e:
         print(f"❌ Error de sintaxis: {e}")
-    # después de generar el AST
-
-#print("\n🌲 Árbol de derivación (AST):")
-#print_ast(ast)
